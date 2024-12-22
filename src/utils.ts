e3c3a3055e1b30b26e48fce4283791930c980bb5
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-12-22 17:21:15 +0600

    feat: add lightbox for images

--- src/utils.ts
+++ src/utils.ts
@@ -15,3 +15,23 @@ export function slugify(text: string): string {
     .replace(/-+$/, '')
     .replace(/-{2,}/, '-');
 }
+
+declare function refreshFsLightbox(): void;
+
+export function addLightboxToBlogPostImages(): void {
+  const blogs = document.querySelectorAll('.blog-content') as NodeListOf<HTMLElement>;
+
+  for (let i = 0; i < blogs.length; i++) {
+    const blog = blogs[i];
+    const images = blog.querySelectorAll('img');
+    for (const image of images) {
+      const a = document.createElement('a');
+      a.href = image.src;
+      a.dataset.fslightbox = `blog-${i}`;
+      image.replaceWith(a);
+      a.appendChild(image);
+    }
+  }
+
+  refreshFsLightbox();
+}

f163e4c3297025be594f66b97d80a5fbdbaf87a9
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-12-23 09:41:59 +0600

    ci: add GitHub actions for deploying and previewing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.WEBSITE_HOST,
+  base: process.env.WEBSITE_BASE,
+
   vite: {
     resolve: {
       alias: {

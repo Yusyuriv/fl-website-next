ba580e5b702cd39e7b77afdf679fd5984a7de040
Yusyuriv <Yusyuriv@users.noreply.github.com>
2024-12-24 09:13:37 +0600

    fix: fix broken builds

--- src/pages/plugins/[slug]/_get-plugin-blog-posts.ts
+++ src/pages/plugins/[slug]/_get-plugin-blog-posts.ts
@@ -1,12 +1,16 @@
 import {getCollection, getEntry} from "astro:content";
 import path from "node:path";
 
-export async function getPluginBlogPosts(pluginPath: string, bySlug = false) {
+export async function getPluginBlogPosts(pluginPath?: string, bySlug = false) {
+  if (!pluginPath) return [];
+
   let directoryPath: string;
   if (!bySlug) {
     directoryPath = path.join(pluginPath, "..", "blog");
   } else {
     const plugin = await getEntry("plugins", pluginPath);
+    if (!plugin) return [];
+    
     directoryPath = path.join(plugin!.filePath!, "..", "blog");
   }
   return getCollection("pluginBlog", v => path.join(v.filePath!).startsWith(directoryPath));

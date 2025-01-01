3fa32c876fc703a8909f4cde1482990a46e20052
Yusyuriv <Yusyuriv@users.noreply.github.com>
2025-01-01 21:53:39 +0600

    fix(theme-builder): fix incorrect link to result icon when deployed to GitHub Pages

--- src/components/theme-builder/theme-demo/Results.svelte
+++ src/components/theme-builder/theme-demo/Results.svelte
@@ -3,19 +3,22 @@ import Result from "@/components/theme-builder/theme-demo/Result.svelte";
 import type {AllState} from "@/components/theme-builder/state/AllState.svelte.js";
 import Scrollbar from "@/components/theme-builder/theme-demo/Scrollbar.svelte";
 import {getContext} from "svelte";
+import {getBaseUrl} from "@/utils.ts";
 
 const state: AllState = getContext("state");
+const baseUrl = getBaseUrl(true);
+const image = `${baseUrl}/favicon.ico`;
 </script>
 
 <div class="results">
-  <Result shortcut={1} image="/favicon.ico" active={state.settings.activeResults[0]}>
+  <Result shortcut={1} {image} active={state.settings.activeResults[0]}>
     {#snippet title()}<i>Hel</i>lo world plugin{/snippet}
   </Result>
-  <Result shortcut={2} image="/favicon.ico" active={state.settings.activeResults[1]}>
+  <Result shortcut={2} {image} active={state.settings.activeResults[1]}>
     {#snippet title()}Hi{/snippet}
     {#snippet subtitle()}Say <i>Hel</i>lo to Flow Launcher{/snippet}
   </Result>
-  <Result shortcut={3} image="/favicon.ico" active={state.settings.activeResults[2]}>
+  <Result shortcut={3} {image} active={state.settings.activeResults[2]}>
     {#snippet title()}<i>Hel</i>lo from the theme creator :){/snippet}
   </Result>
   <Result shortcut={4} glyph="W" active={state.settings.activeResults[3]}>

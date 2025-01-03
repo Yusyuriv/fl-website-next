3ab6e018235444eb168dcfef28d1a0abc12135ba
Yusyuriv <Yusyuriv@users.noreply.github.com>
2025-01-03 19:53:45 +0600

    fix(plugins): change the way plugin blog posts are looked up

--- src/pages/plugins/[slug]/_get-plugin-blog-posts.ts
+++ src/pages/plugins/[slug]/_get-plugin-blog-posts.ts
@@ -1,17 +1,12 @@
-import {getCollection, getEntry} from "astro:content";
+import {getCollection} from "astro:content";
 import path from "node:path";
 
-export async function getPluginBlogPosts(pluginPath?: string, bySlug = false) {
-  if (!pluginPath) return [];
+export async function getPluginBlogPosts(pluginId?: string) {
+  if (!pluginId) return [];
 
-  let directoryPath: string;
-  if (!bySlug) {
-    directoryPath = path.join(pluginPath, "..", "blog");
-  } else {
-    const plugin = await getEntry("plugins", pluginPath);
-    if (!plugin) return [];
-    
-    directoryPath = path.join(plugin!.filePath!, "..", "blog");
-  }
+  const plugin = await getCollection("plugins", v => v.data.id === pluginId).then(v => v[0]);
+  if (!plugin) return [];
+
+  const directoryPath = path.join(plugin!.filePath!, "..", "blog");
   return getCollection("pluginBlog", v => path.join(v.filePath!).startsWith(directoryPath));
 }
